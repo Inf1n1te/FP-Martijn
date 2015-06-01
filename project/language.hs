f8a8c6a07eb7d477f48f6e96270d4f1659bcdbf2
Inf1n1te <t.kerkhoven@student.utwente.nl>
2015-06-01 14:45:58 +0200

    fixed

--- project/language.hs
+++ project/language.hs
@@ -1,14 +1,13 @@
 type Program 	= [Clause]
 
-type Clause	= Expression [Expression]
+type Clause	= (Expression, [Expression])
 
 type Expression = String
 
 defaultProgram :: Program
-defaultProgram =  ["a0", "a1", "a2", "b0" ["a0", "a1"], "b1" ["a1", "a2"], "b2" ["a1", "a2", "d"], "c0" ["b0", "b1"], "c1" ["b0", "b1", "b2"]]
+defaultProgram =  [("a0",[]), ("a1",[]), ("a2",[]), ("b0", ["a0", "a1"]), ("b1", ["a1", "a2"]), ("b2", ["a1", "a2", "d"]), ("c0", ["b0", "b1"]), ("c1", ["b0", "b1", "b2"])]
 
 evalProp :: Program -> Program -> Expression -> Bool
-evalProp [] e _			= error "Empty base program"
 evalProp [] _ _			= False
 evalProp _ [] _			= False
 evalProp p (c@(e, n):cs) q	| e == q && n == []	= True
