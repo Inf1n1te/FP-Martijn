7cea76c2920f23325d50e9e34c187d5b27ea69aa
MartijnVerkleij <mail@mverkleij.nl>
2015-06-01 15:29:09 +0200

    eval1prop base impementation, now bufixes

--- project/eval2prop.hs
+++ project/eval2prop.hs
@@ -6,4 +6,17 @@ type Program = [Clause]
 type Clause = (Expression, [Expression])
 type Expression = (String, Argument)
 
-evalOne :: Program -> Program -> Expression -> Either (Bool, [Expressions])
+evalOne :: Program -> Program -> Expression -> (Either Bool [Expression])
+
+evalOne [] _ (_, Constant _) 		= Left False
+evalOne _ [] (_, Constant _) 		= Left False
+evalOne [] _ (_, Variable _) 		= Right []
+evalOne _ [] (_, Variable _) 		= Right []
+
+evalOne p (c@(e@(s,x), n):cs) y@(q, Constant a) 
+	| e == y && n == []	= Left True
+	| e == y		= all (\(Left x) -> x == True) $ map (evalOne p p) n
+	| s == q 		= all (\(Left x) -> x == True) ( map (evalOne p p) (map (\(x,_) -> (x,a)) n) )
+	| otherwise		= evalOne p cs y 
+
+--evalOne p (c:cs) (q, Variable a) = 
