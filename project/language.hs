aa3f09fbbcc0c1015fc36f9244e4180acd20c692
MartijnVerkleij <mail@mverkleij.nl>
2015-06-01 14:31:52 +0200

    Eval basisimplementatie

--- project/language.hs
+++ project/language.hs
@@ -1,12 +1,14 @@
 type Program 	= [Clause]
 
-data Clause	= Expression (Maybe Predicate)
-
-type Predicate	= [Expression]
-
-type Target	= Expression
+data Clause	= Expression [Expression]
+	deriving (Eq, Show)
 
 data Expression = String
+	deriving (Eq, Show)
 
 
 
+evalProp :: Program -> Program -> Expression -> Bool
+evalProp p (c@(e n):cs) q	| e == q && n = []	= True
+				| e == q 		= all (== True) $ map (evalProp p p) n
+				| otherwise		= evalProp p cs e
