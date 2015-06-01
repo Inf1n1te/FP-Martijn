32095a4233b59b8b81d979fe7c23e06e27fd13ae
Inf1n1te <t.kerkhoven@student.utwente.nl>
2015-06-01 14:38:52 +0200

    strings

--- project/language.hs
+++ project/language.hs
@@ -5,4 +5,4 @@ data Clause	= Expression [Expression]
 data Expression = String
 
 defaultProgram :: Program
-defaultProgram =  [a0, a1, a2, b0 [a0, a1], b1 [a1, a2], b2 [a1, a2, d], c0 [b0, b1], c1 [b0, b1, b2]]
+defaultProgram =  ["a0", "a1", "a2", "b0" ["a0", "a1"], "b1" ["a1", "a2"], "b2" ["a1", "a2", "d"], "c0" ["b0", "b1"], "c1" ["b0", "b1", "b2"]]
