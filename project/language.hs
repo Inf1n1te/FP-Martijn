0da927cc5b89421bd10de8db9b314affb90f938f
Inf1n1te <t.kerkhoven@student.utwente.nl>
2015-06-01 14:15:15 +0200

    default program

--- project/language.hs
+++ project/language.hs
@@ -1,12 +1,8 @@
 type Program 	= [Clause]
 
-data Clause	= Expression (Maybe Predicate)
-
-type Predicate	= [Expression]
-
-type Target	= Expression
+data Clause	= Expression [Expression]
 
 data Expression = String
 
-
-
+defaultProgram :: Program
+defaultProgram =  [a0, a1, a2, b0 [a0, a1], b1 [a1, a2], b2 [a1, a2, d], c0 [b0, b1], c1 [b0, b1, b2]]
