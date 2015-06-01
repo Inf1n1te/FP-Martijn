f92dc6bd08402707fceb0e186778fd2f813fc5c6
Inf1n1te <t.kerkhoven@student.utwente.nl>
2015-06-01 15:03:46 +0200

    default Program

--- project/eval2prop.hs
+++ project/eval2prop.hs
@@ -1,9 +1,21 @@
 data Argument 	= Variable String
-		| Constant String
+				| Constant String
 	deriving (Eq,Show)
 
 type Program = [Clause]
 type Clause = (Expression, [Expression])
 type Expression = (String, Argument)
+
+defaultProgram :: Program
+defaultProgram = [
+	(("p", Constant "a"), []), 
+	(("p", Constant "b"), []), 
+	(("p", Constant "c"), []), 
+	
+	(("q", Constant "a"), []), 
+	(("q", Constant "b"), []), 
+	
+	(("r", Variable "X"), [("p", Variable "X"), ("q", Variable "X")])
+	]
 
 evalOne :: Program -> Program -> Expression -> Either (Bool, [Expressions])
