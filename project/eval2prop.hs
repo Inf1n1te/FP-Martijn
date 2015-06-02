278d56fd8fb6f7455fc9329277eb1968bfad5e1a
Inf1n1te <t.kerkhoven@student.utwente.nl>
2015-06-02 14:28:18 +0200

    fixes en stuff

--- project/eval2prop.hs
+++ project/eval2prop.hs
@@ -1,3 +1,5 @@
+module Eval2prop where
+
 data Argument 	= Variable String
 				| Constant String
 	deriving (Eq,Show)
@@ -8,26 +10,26 @@ type Expression = (String, Argument)
 
 defaultProgram :: Program
 defaultProgram = [
-	(("p", Constant "a"), []), 
-	(("p", Constant "b"), []), 
-	(("p", Constant "c"), []), 
-	
-	(("q", Constant "a"), []), 
-	(("q", Constant "b"), []), 
-	
+	(("p", Constant "a"), []),
+	(("p", Constant "b"), []),
+	(("p", Constant "c"), []),
+
+	(("q", Constant "a"), []),
+	(("q", Constant "b"), []),
+
 	(("r", Variable "X"), [("p", Variable "X"), ("q", Variable "X")])
 	]
-evalOne :: Program -> Program -> Expression -> (Either Bool [Expression])
+evalOne :: Program -> Program -> Expression -> Either Bool [Expression]
 
 evalOne [] _ (_, Constant _) 		= Left False
 evalOne _ [] (_, Constant _) 		= Left False
 evalOne [] _ (_, Variable _) 		= Right []
 evalOne _ [] (_, Variable _) 		= Right []
 
-evalOne p (c@(e@(s,x), n):cs) y@(q, Constant a) 
+evalOne p (c@(e@(s,x), n):cs) y@(q, a@(Constant _))
 	| e == y && n == []	= Left True
 	| e == y		= Left $ all (== Left True) $ map (evalOne p p) n
-	| s == q 		= Left $ all (== Left True) ( map (evalOne p p) (map (\(x,_) -> (x, Constant a)) n) )
-	| otherwise		= evalOne p cs y 
+	| s == q 		= Left $ all (== Left True) ( map (evalOne p p) (map (\(z,_) -> (z, a)) n) )
+	| otherwise		= evalOne p cs y
 
---evalOne p (c:cs) (q, Variable a) = 
+--evalOne p (c:cs) (q, Variable a) =
