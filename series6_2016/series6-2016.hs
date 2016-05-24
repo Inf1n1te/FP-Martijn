053912c49e3efc3e3a821bc9655000baa84d3831
Martijn Verkleij <github@mverkleij.nl>
2016-05-24 14:57:41 +0200

    working tuples

--- series6_2016/series6-2016.hs
+++ series6_2016/series6-2016.hs
@@ -4,10 +4,12 @@ import FPPrac.Trees
 import Data.Map
 
 data Expr   = Const Int
+            | Tuple Expr Expr
+            | Triple Expr Expr Expr
             | Boolean Bool
             | Var String
             | BinOp String Expr Expr
-            | If Expr Then [Expr] Else [Expr]
+            | If Expr Expr Expr
             | App Expr Expr
     deriving Show
 data Type   = IntType 
@@ -38,6 +40,10 @@ typeOf :: Env -> Expr -> Type
 typeOf env (Const _)        = IntType
 typeOf env (Boolean _)      = BoolType
 typeOf env (Var str)        = (fromList env)!str
+typeOf env (Tuple e1 e2)    | typeOf env e1 == typeOf env e2    = typeOf env e1
+                            | otherwise                         = error "Tuple has mixed type"
+typeOf env (Triple e1 e2 e3)| typeOf env e1 == typeOf env e2 && typeOf env e1 == typeOf env e3  = typeOf env e1
+                            | otherwise                                                         = error "Triple has mixed type"
 typeOf env (BinOp op e1 e2) = case t_op of 
     FunType t0 (FunType t1 t2)
         | t0 == t_e1 && t1 == t_e2  -> t2
