230057faaf0e32bf6bcb278862350575cb4f05e0
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2016-05-24 12:27:40 +0200

    6b stuff

--- series6_2016/series6-2016.hs
+++ series6_2016/series6-2016.hs
@@ -1,13 +1,18 @@
 module FP_Grammar where
 
 import FPPrac.Trees
+import Data.Map
 
 data Expr   = Const Int
+            | Boolean Bool
             | Var String
             | BinOp String Expr Expr
             | App Expr Expr
+    deriving Show
 data Type   = IntType 
+            | BoolType
             | FunType Type Type
+    deriving Show
 
 type Env = [(String, Type)]
 
@@ -16,9 +21,21 @@ env =   [("+", FunType IntType (FunType IntType IntType))
         ,("-", FunType IntType (FunType IntType IntType))
         ,("*", FunType IntType (FunType IntType IntType))
 
+        ,("&&", FunType BoolType (FunType BoolType BoolType))
+        ,("||", FunType BoolType (FunType BoolType BoolType))
+
         ,("x", IntType)
+        ,("y", IntType)
+        ,("z", IntType)
         ]
 
 
 typeOf :: Env -> Expr -> Type
-typeOf env expr 
+typeOf env (Const _)        = IntType
+typeOf env (Boolean _)      = BoolType
+typeOf env (Var str)        = map!str
+    where
+        map = fromList env
+typeOf env (BinOp str _ _)  = map!str
+    where
+        map = fromList env
