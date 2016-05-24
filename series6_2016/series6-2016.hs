b76b4d9388858b4c4fa748d2a9bc79f3c75cb57b
Martijn Verkleij <github@mverkleij.nl>
2016-05-24 15:35:18 +0200

    Added tuples, triples, operations on those

--- series6_2016/series6-2016.hs
+++ series6_2016/series6-2016.hs
@@ -13,19 +13,40 @@ data Expr   = Const Int
             | App Expr Expr
     deriving Show
 data Type   = IntType 
+            | TupleType Type Type
+            | TripleType Type Type Type
             | BoolType
             | FunType Type Type
     deriving (Show, Eq)
+
+--type (TupleType IntType IntType) = TupleType IntType IntType
+--type (TupleType BoolType BoolType) = TupleType BoolType BoolType
+--type (TripleType IntType IntType IntType) = TripleType IntType IntType IntType
+--type (TripleType BoolType BoolType BoolType) = TripleType BoolType BoolType BoolType
 
 type Env = [(String, Type)]
 env :: Env
 env =   [("+", FunType IntType (FunType IntType IntType))
         ,("-", FunType IntType (FunType IntType IntType))
         ,("*", FunType IntType (FunType IntType IntType))
-
         ,("&&", FunType BoolType (FunType BoolType BoolType))
         ,("||", FunType BoolType (FunType BoolType BoolType))
+        
+        ,("++", FunType (TupleType IntType IntType) (FunType (TupleType IntType IntType) (TupleType IntType IntType)))
+        ,("--", FunType (TupleType IntType IntType) (FunType (TupleType IntType IntType) (TupleType IntType IntType)))
+        ,("**", FunType (TupleType IntType IntType) (FunType (TupleType IntType IntType) (TupleType IntType IntType)))
+        ,("&&&", FunType (TupleType BoolType BoolType) (FunType (TupleType BoolType BoolType) (TupleType BoolType BoolType)))
+        ,("|||", FunType (TupleType BoolType BoolType) (FunType (TupleType BoolType BoolType) (TupleType BoolType BoolType)))
 
+        ,("+++", FunType (TripleType IntType IntType IntType) (FunType (TripleType IntType IntType IntType) (TripleType IntType IntType IntType)))
+        ,("---", FunType (TripleType IntType IntType IntType) (FunType (TripleType IntType IntType IntType) (TripleType IntType IntType IntType)))
+        ,("***", FunType (TripleType IntType IntType IntType) (FunType (TripleType IntType IntType IntType) (TripleType IntType IntType IntType)))
+        ,("&&&&", FunType (TripleType BoolType BoolType BoolType) (FunType (TripleType BoolType BoolType BoolType) (TripleType BoolType BoolType BoolType)))
+        ,("||||", FunType (TripleType BoolType BoolType BoolType) (FunType (TripleType BoolType BoolType BoolType) (TripleType BoolType BoolType BoolType)))
+        
+        ,("+*&&", FunType (TripleType IntType IntType BoolType) (FunType (TripleType IntType IntType BoolType) (TripleType IntType IntType BoolType)))
+        
+        
         ,("x", IntType)
         ,("y", IntType)
         ,("z", IntType)
@@ -34,16 +55,16 @@ env =   [("+", FunType IntType (FunType IntType IntType))
         ,("b", BoolType)
         ,("c", BoolType)
         ]
+        
+testTriplePlus = typeOf env (BinOp "+++" (Triple (Const 5) (Const 3) (Const 12)) (Triple (Const 22) (Var "x") (Var "y")))
 
 
 typeOf :: Env -> Expr -> Type
 typeOf env (Const _)        = IntType
 typeOf env (Boolean _)      = BoolType
 typeOf env (Var str)        = (fromList env)!str
-typeOf env (Tuple e1 e2)    | typeOf env e1 == typeOf env e2    = typeOf env e1
-                            | otherwise                         = error "Tuple has mixed type"
-typeOf env (Triple e1 e2 e3)| typeOf env e1 == typeOf env e2 && typeOf env e1 == typeOf env e3  = typeOf env e1
-                            | otherwise                                                         = error "Triple has mixed type"
+typeOf env (Tuple e1 e2)    = TupleType (typeOf env e1) (typeOf env e2)
+typeOf env (Triple e1 e2 e3)= TripleType (typeOf env e1) (typeOf env e2) (typeOf env e3)
 typeOf env (BinOp op e1 e2) = case t_op of 
     FunType t0 (FunType t1 t2)
         | t0 == t_e1 && t1 == t_e2  -> t2
