362182c29cd0092844ae7e1f936f8d0e91ed69f3
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2016-05-24 11:22:54 +0200

    6b

--- series6_2016/series6-2016.hs
+++ series6_2016/series6-2016.hs
@@ -1,2 +1,24 @@
+module FP_Grammar where
+
 import FPPrac.Trees
-import FP_Grammar
+
+data Expr 	= Const Int
+			| Var String
+			| BinOp String Expr Expr
+			| App Expr Expr
+data Type 	= IntType 
+			| FunType Type Type
+
+type Env = [(String, Type)]
+
+env :: Env
+env = 	[("+", FunType IntType (FunType IntType IntType))
+		,("-", FunType IntType (FunType IntType IntType))
+		,("*", FunType IntType (FunType IntType IntType))
+
+		,("x", IntType)
+		]
+
+
+typeOf :: Env -> Expr -> Type
+typeOf env 
