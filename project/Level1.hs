19ceef5a1d2faf8d1ee36ddeeb9b349e30bcdca8
Martijn Verkleij <github@mverkleij.nl>
2016-05-30 16:25:01 +0200

    working on backtrCKING

--- project/Level1.hs
+++ project/Level1.hs
@@ -1,18 +1,46 @@
 module Level1 where
 
+import Debug.Trace
+
 -- Imports
 
 
 -- Data types
 
-type Atom		= String
-type Clause		= (Atom, [Atom])
+data Term		= Atom String
+    deriving (Show, Eq)
+type Clause		= (Term, [Term])
 type Program	= [Clause]
-type Query		= [Atom]
+type Query		= [Term]
 
+
+-- Test programs
+
+program1 = [(Atom "a", []), 
+            (Atom "b", [Atom "c", Atom "d"]), 
+            (Atom "b", [Atom "c", Atom "e"]), 
+            (Atom "c", []),
+            (Atom "d", [Atom "f"]),
+            (Atom "e", [])]
+            
+program2 = [(Atom "a", []), 
+            (Atom "b", [Atom "c", Atom "d"]), 
+            (Atom "b", [Atom "c", Atom "e"]), 
+            (Atom "c", [Atom "d"]),
+            (Atom "d", [Atom "f"]),
+            (Atom "e", [])]
 
 -- Functions
 evalProp :: Program -> Query -> Bool
 evalProp [] _ 			= False
 evalProp _ []			= True
-evalProp program query 	=
+evalProp program query@(queryAtomHead:queryAtoms) = head
+    [
+        evalProp program (clauseAtoms ++ queryAtoms) | 
+        clause@(clauseAtom, clauseAtoms) <- program,
+        
+        trace ("query: "++ (show queryAtomHead) ++ " -> " ++ (show queryAtoms) ++ " rule: " ++ (show clauseAtom) ++ " -> "++ (show clauseAtoms))
+        
+        clauseAtom == queryAtomHead]
+        
+        
