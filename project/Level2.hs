70b66d883b26571c7a6fd961a0240cdfa37cdaa6
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2016-05-31 11:08:36 +0200

    todo and basic types

--- project/Level2.hs
+++ project/Level2.hs
@@ -4,6 +4,21 @@ module Level2 where
 
 
 -- Data types
+data Term			= Constant String | Variable String
+    deriving (Show, Eq)
+type Atom			= (String, Term)
+type Clause			= (Atom, [Atom])
+type Program		= [Clause]
+type Query			= [Atom]
+type Substitution	= (Term, Term)
 
+-- Substitution operation in type classes
 
 -- Functions
+
+-- Rename function
+
+-- Unify function
+
+-- evalOne function
+
