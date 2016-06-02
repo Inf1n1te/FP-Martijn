3ead2cd21a9d47fcea49ed32d5179af0fafda69e
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2016-06-02 14:20:25 +0200

    substitution

--- project/Level3.hs
+++ project/Level3.hs
@@ -16,3 +16,43 @@ type Substitution   = (Term, Term)
 -- Functions
 
 
+{- Substitution operation in type classes
+
+Usage:
+Term/Atom <~ Substitution
+
+-}
+class Substitute a where
+    (<~) :: a -> Substitution -> a
+
+instance Substitute Term where
+    -- Substitute a variable with a Term
+    term <~ (original@(Variable _), replacement)
+        | term == original  = replacement
+        | otherwise         = term
+    -- Substitute a constant with a Term
+    term <~ (original@(Constant _), replacement)
+        | term == original && original == replacement   = replacement
+        | original == replacement                       = term
+        | otherwise = error "Cannot substitute a constant"
+
+instance Substitute [Term] where
+	-- Substitute in a list of Terms
+	terms <~ substitution = map (<~ substitution) terms
+
+instance Substitute Atom where
+    -- Substitute a variable with a Term
+    (predicate, terms) <~ substitution = (predicate, terms <~ substitution)
+
+instance Substitute [Atom] where
+     -- Substitute a variable with a Term for all variables in a list of atoms
+    atoms <~ substitution = map (<~ substitution) atoms
+
+instance Substitute Clause where
+    -- Substitute a variable with a Term for all variables in a clause
+    (atom, atoms) <~ substitution 
+        = (atom <~ substitution, atoms <~ substitution)
+
+instance Substitute Program where
+	-- Substitute in a program
+	program <~ substitution = map (<~ substitution) program
