d0f9a6c4eae774655dc133685e5e7c715abcb204
Martijn Verkleij <github@mverkleij.nl>
2016-06-02 14:12:28 +0200

    defined data types for evalMulti

--- project/Level3.hs
+++ project/Level3.hs
@@ -4,6 +4,15 @@ module Level3 where
 
 
 -- Data types
+data Term           = Constant String | Variable String
+    deriving (Show, Eq)
+type Atom           = (String, [Term])
+type Clause         = (Atom, [Atom])
+type Program        = [Clause]
+type Query          = [Atom]
+type Substitution   = (Term, Term)
 
 
 -- Functions
+
+
